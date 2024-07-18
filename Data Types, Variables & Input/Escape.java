c6ce37bb25c1ee9aa127e76d5447e48cb1a9bccd
Kalash Jain <kalashjain2201@gmail.com>
2024-07-18 21:54:52 +0530

    Create Escape.java

---
+++ Data Types, Variables & Input/Escape.java
@@ -0,0 +1,11 @@
+public class Escape {
+    public static void main(String[] args) {
+        System.out.println("Hello 'KG Coding'...");
+        System.out.println("Hello \'KG Coding\'...");
+        System.out.println("Hello \"KG Coding\"...");
+        System.out.println("Hello \\KG Coding\\...");
+        System.out.println("Hello \nKG Coding...");
+        System.out.println("Hello \tKG Coding...");
+        System.out.println("Hello \bKG Coding...");
+    }
+}
