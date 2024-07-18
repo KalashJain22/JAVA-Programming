2747999bc9d34b363749d13a495761ec1b3412b3
Kalash Jain <kalashjain2201@gmail.com>
2024-07-18 21:52:02 +0530

    Create Variables.java

---
+++ Data Types, Variables & Input/Variables.java
@@ -0,0 +1,22 @@
+public class Variables {
+    public static void main(String[] args) {
+        int myNumber = 467;
+        int yourNumber;
+        yourNumber = 467;
+        System.out.println(yourNumber);
+        yourNumber = 34;
+        System.out.println(yourNumber);
+
+        float myFloat= 5.0F;
+        double myDouble=4.453463;
+        System.out.println(myFloat);
+
+        boolean isVegetarian = true;
+        System.out.println(isVegetarian);
+
+        String wishes = "Good Morning";
+        System.out.println(wishes);
+
+        char myCharacter = 'k';
+    }
+}
