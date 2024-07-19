f8d1ecb9fc5859c096083b18aebc93c64f577b23
Kalash Jain <kalashjain2201@gmail.com>
2024-07-19 11:13:28 +0530

    Create Arithmetic.java

---
+++ Operator, If-else & Number System/Arithmetic.java
@@ -0,0 +1,20 @@
+public class Arithmetic {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 3;
+        
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+
+        double x = 8;
+        double y = 7;
+        
+        System.out.println(x+y);
+        System.out.println(x-y);
+        System.out.println(x*y);
+        System.out.println(x/y); 
+    }
+}
