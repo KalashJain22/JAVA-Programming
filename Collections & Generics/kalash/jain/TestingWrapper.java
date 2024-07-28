bab5196ff796f0a5d9fef00076a2d53b08064b81
Kalash Jain <kalashjain2201@gmail.com>
2024-07-28 20:34:00 +0530

    Create TestingWrapper.java

---
+++ Collections & Generics/kalash/jain/TestingWrapper.java
@@ -0,0 +1,9 @@
+package kalash.jain;
+
+public class TestingWrapper {
+    public static void main(String[] args) {
+        Integer first = Integer.valueOf("55");
+        Integer second = 5;  //Autoboxing
+        int third = second;  //Unboxing
+    }
+}
